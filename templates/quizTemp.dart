455ff7f72083f1c80ecd04bd0bef1c7639ff0cc2
AishwaryaArun15 <57674575+AishwaryaArun15@users.noreply.github.com>
2020-12-27 16:28:20 +0530

    Update quizTemp.dart

--- templates/quizTemp.dart
+++ templates/quizTemp.dart
@@ -5,18 +5,17 @@ import './question.dart';
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
   final int questionIndex;
-  // final int resultValue;
   final Function answerQuestion;
 
   Quiz(
     this.questions,
     this.answerQuestion,
     this.questionIndex,
-    // @required this.resultValue,
   );
 
   @override
   Widget build(BuildContext context) {
-    return  Container(child: Text("Hello from Quiz"))
+    return  Container(child: Text("Hello from Quiz"));
+    // Calling Question and Answer widgets
   }
 }
